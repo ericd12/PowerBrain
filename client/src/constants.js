2f67212b148f5b131816db5f5c059dc64f3929ce
SamsonPit12 <edressler6@gmail.com>
2020-07-23 19:34:35 -0400

    .

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "//ec2-100-25-181-86.compute-1.amazonaws.com";
+export const API_URL = "//ec2-52-207-202-20.compute-1.amazonaws.com"
 
 export const BLANK_ELEMENT = {
   elementCategory: "",
