8d0821570f499077fa417daee744afdea3cdee72
SamsonPit12 <edressler6@gmail.com>
2020-07-23 20:11:24 -0400

    stuff

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "client/build")));
 
 //testing
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
