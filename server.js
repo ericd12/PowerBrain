7e771d19e58078a51c6e3b77d21181af6126075c
Eric D <Edressler6@gmail.com>
2020-07-25 14:21:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 app.use(express.static('client/build'));
 
-app.get(['/', '/*'], (req,res) => {
+app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
