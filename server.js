dbf0ed8d7eb83ab490719aa4e33ed3aa0088eaed
Eric D <Edressler6@gmail.com>
2020-07-25 14:51:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 //testing
 
